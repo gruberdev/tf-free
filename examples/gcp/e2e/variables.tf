966fafb21c672db2a84b96190541edb1f1634068
gruberx <contact@tulpas.dev>
2021-05-24 17:19:48 -0300

    [gcp] added google project to variable

--- examples/gcp/e2e/variables.tf
+++ examples/gcp/e2e/variables.tf
@@ -12,3 +12,9 @@ variable "gcp_project_region" {
   description = "GCP Network Name. [Oficial GCP Documentation](https://cloud.google.com/compute/docs/machine-types) - [Terraform provider Documentation](https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_instance#machine_type)"
 }
 
+variable "google_project" {
+  type        = string
+  default     = ""
+  description = "GCP Network Name. [Oficial GCP Documentation](https://cloud.google.com/compute/docs/machine-types) - [Terraform provider Documentation](https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_instance#machine_type)"
+}
+
