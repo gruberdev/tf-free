90e503e084b93b0e13ba93518a46c7afe3c0a1ca
gruberx <contact@tulpas.dev>
2021-05-24 17:28:44 -0300

    //

--- examples/gcp/e2e/main.tf
+++ examples/gcp/e2e/main.tf
@@ -2,6 +2,6 @@ module "gcp_machine" {
   source         = "github.com/gruberdev/tf-free/modules/gcp"
   project_region = var.gcp_project_region
   instance_name  = var.gcp_instance_name
-  project        = var.google_project
+  google_project = var.google_project
 }
 
