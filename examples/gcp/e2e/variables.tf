8a119d9cd1dd43eae4843e46145f8aba50efb93b
gruberx <contact@tulpas.dev>
2021-05-24 17:18:47 -0300

    [gcp] fixed variables attribution

--- examples/gcp/e2e/variables.tf
+++ examples/gcp/e2e/variables.tf
@@ -1,12 +1,12 @@
 
 
-variable "instance_name" {
+variable "gcp_instance_name" {
   type        = string
   default     = "example-machine"
   description = "GCP Network Name. [Oficial GCP Documentation](https://cloud.google.com/compute/docs/machine-types) - [Terraform provider Documentation](https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_instance#machine_type)"
 }
 
-variable "project_region" {
+variable "gcp_project_region" {
   type        = string
   default     = "us-west1"
   description = "GCP Network Name. [Oficial GCP Documentation](https://cloud.google.com/compute/docs/machine-types) - [Terraform provider Documentation](https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_instance#machine_type)"
