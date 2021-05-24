9fc9d8d87c6baff7bb2af33f418783f6c89783d2
gruberx <contact@tulpas.dev>
2021-05-24 20:39:41 -0300

    [gcp] added dependency

--- modules/gcp/main.tf
+++ modules/gcp/main.tf
@@ -6,11 +6,18 @@ module "gcp_vpc" {
 }
 
 module "gcp_firewall" {
+  depends_on = [
+    module.gcp_vpc.network_name
+  ]
   source       = "github.com/gruberdev/tf-free/modules/gcp/firewall"
   network_name = module.gcp_vpc.network_name
 }
 
 module "gcp_instance" {
+  depends_on = [
+    module.gcp_vpc.network_name,
+    module.gcp_vpc.ipv4_add
+  ]
   source       = "github.com/gruberdev/tf-free/modules/gcp/compute"
   name         = var.instance_name
   region       = var.project_region
