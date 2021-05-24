5211139d1692b53733811609dcde4c5e67dc9c86
gruberx <contact@tulpas.dev>
2021-05-24 17:30:24 -0300

    aa

--- modules/gcp/vpc/main.tf
+++ modules/gcp/vpc/main.tf
@@ -1,7 +1,7 @@
 resource "google_compute_network" "default" {
   name                    = var.vpc_name
   auto_create_subnetworks = "true"
-  project                 = var.google_zproject
+  project                 = var.google_project
 }
 
 resource "google_compute_address" "static" {
