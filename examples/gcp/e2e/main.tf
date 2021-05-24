cf355424333475373b3bdedcc085c7b50d99b2a1
gruberx <contact@tulpas.dev>
2021-05-24 16:25:03 -0300

    [gcp] added module changes to gcp

--- examples/gcp/e2e/main.tf
+++ examples/gcp/e2e/main.tf
@@ -1,5 +1,5 @@
 module "gcp_machine" {
-  source = "github.com/gruberdev/tf-free/modules/gcp"
+  source         = "github.com/gruberdev/tf-free/modules/gcp"
+  project_region = var.gcp_project_region
+  instance_name  = var.gcp_instance_name
 }
-
-
