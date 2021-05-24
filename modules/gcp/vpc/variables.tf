7c8e66480fae0d1d6f38d15bfeacfaeda433b301
gruberx <contact@tulpas.dev>
2021-05-24 16:57:49 -0300

    [gcp] removed all validate

--- modules/gcp/vpc/variables.tf
+++ modules/gcp/vpc/variables.tf
@@ -2,10 +2,6 @@ variable "vpc_name" {
   type        = string
   default     = "gcp-internal-network"
   description = "Your instance's network on GCP. [GCP's Official documentation on naming resources](https://cloud.google.com/compute/docs/naming-resources#resource-name-format)"
-  validation {
-    condition     = can(regex("[a-z]([-a-z0-9]*[a-z0-9])?", var.vpc_name))
-    error_message = "Error: Your network name is invalid, adjust it to GCP's resources naming convention."
-  }
 }
 
 variable "static_ip_name" {
