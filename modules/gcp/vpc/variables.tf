e1bd6a1f7126ba7c7008df5d7c5546c891b610af
gruberx <contact@tulpas.dev>
2021-05-24 16:55:22 -0300

    [gcp] removed validation

--- modules/gcp/vpc/variables.tf
+++ modules/gcp/vpc/variables.tf
@@ -12,10 +12,6 @@ variable "static_ip_name" {
   type        = string
   default     = "ipv4-main-ip"
   description = "Your static IP network resource name on GCP. [GCP's Official documentation on naming resources](https://cloud.google.com/compute/docs/naming-resources#resource-name-format)"
-  validation {
-    condition     = can(regex("[a-z]([-a-z0-9]*[a-z0-9])?", var.static_ip_name))
-    error_message = "Error: Your static IP address resource name is invalid, adjust it to GCP's resources naming convention."
-  }
 }
 
 variable "subnetworks" {
