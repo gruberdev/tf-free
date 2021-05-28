85570db50c940d4b6d1143eb55341e6e5250a023
gruberx <contact@tulpas.dev>
2021-05-28 04:35:25 -0300

    [docs] docs basic structure

--- .codedoc/config.ts
+++ .codedoc/config.ts
@@ -5,9 +5,16 @@ import { theme } from './theme';
 
 
 export const config = /*#__PURE__*/configuration({
-  theme,                                  // --> add the theme. modify `./theme.ts` for changing the theme.
+  theme,
+  src: {
+    base: 'docs/md',                     // --> the base folder for all markdowns
+    toc: '_toc.md',                      // --> markdown file for toc, relative to `base`
+    not_found: '404.md',                // --> markdown file for 404 page, relative to `base`
+    pick: /\.md$/,                       // --> which files to pick (default: .md files)
+    drop: /(^_)|(\/_)/,                  // --> which files to drop (default: _something.md files)               // --> markdown file for 404 page, relative to `base`
+  },                                // --> add the theme. modify `./theme.ts` for changing the theme.
   dest: {
-    namespace: '.',
+    namespace: '',
     html: 'docs/build',
     assets: 'docs/build',
     bundle: 'docs/build/assets',
@@ -15,9 +22,14 @@ export const config = /*#__PURE__*/configuration({
   },
   page: {
     title: {
-      base: 'tf-free | Documentation'                     // --> the base title of your doc pages
-    }
+      base: 'tf-free | Documentation'                    // --> the base title of your doc pages
+    },
+
   },
+  dev: {
+    port: 3000                           // --> the port for local dev server
+  },
+
   misc: {
     github: {
       user: 'gruberdev',                  // --> your github username (where your repo is hosted)
--- docs/md/_toc.md
+++ docs/md/_toc.md
@@ -2,21 +2,52 @@
 
 [Getting started](/docs/setup/getting-started)
 
+> :Collapse label="Project Configuration"
+>
+> [Configuring your setup](/docs/intro)
+
+> :Collapse label=Cloud Providers
+>
+> > :Collapse label=Google Cloud Platform
+> >
+> > [Available Resources](/docs/providers/gcp/sample)
+> >
+> > [Configuring](/docs/code-features)
+> >
+> > [Examples](/docs/code-features)
+> >
+> > [References](/docs/code-features)
+>
+> > :Collapse label=Amazon Web Services
+> >
+> > [Available Resources](/docs/providers/gcp/sample)
+> >
+> > [Configuration Options](/docs/code-features)
+> >
+> > [Examples](/docs/code-features)
+> >
+> > [References](/docs/code-features)
+>
+> > :Collapse label=Microsoft Azure Cloud
+> >
+> > [Available Resources](/docs/providers/gcp/sample)
+> >
+> > [Configuration Options](/docs/code-features)
+> >
+> > [Examples](/docs/code-features)
+> >
+> > [References](/docs/code-features)
+
 > :Collapse label="About"
+> 
+> [Awknoeledgesm](/docs/intro/what-is)
+>
+> [What is this project?](/docs/intro/what-is)
 >
 > [What is this project?](/docs/intro/what-is)
 >
 > [Whose was it made for?](/docs/intro/about)
 >
-> [How can I setup it?](/docs/intro/next-steps)
-
-> :Collapse label=Google Cloud
->
-> [Markdown Cheat Sheet](/docs/cheat-sheet)
->
-> [Code Features](/docs/code-features)
+> [How can I contribute?](/docs/intro/next-steps)
 
 <br><br>
-
-To add links to your other documents, simply
-modify contents of `docs/md/_toc.md`.
